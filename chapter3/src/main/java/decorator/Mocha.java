8fd7675605cf601daa09b5129f0377c056b7db64
voronyansky <voronyansky.egor@yandex.ru>
2016-06-25 23:14:13 +0400

    Decorators for addition

--- chapter3/src/main/java/decorator/Mocha.java
+++ chapter3/src/main/java/decorator/Mocha.java
@@ -1,21 +1,33 @@
 package decorator;
 
 /**
- * Created by Egor on 24.06.2016.
+ * It is decorator for mocha addition
  */
 public class Mocha extends CondimentDecorator {
 
     private Beverage beverage;
 
+    /**
+     * Create an instance of mocha addition
+     * @param beverage - instance of beverage class
+     */
     public Mocha(Beverage beverage) {
         this.beverage = beverage;
     }
 
+    /**
+     * Return a description about this addition
+     * @return - String describes about this addition
+     */
     @Override
     public String getDescription() {
         return beverage.getDescription()+ ", mocha";
     }
 
+    /**
+     * It method calls for calculate price
+     * @return - cost of mocha addition
+     */
     @Override
     public double cost() {
         return 0.20 + beverage.cost();
--- chapter3/src/main/java/decorator/Whip.java
+++ chapter3/src/main/java/decorator/Whip.java
@@ -1,20 +1,34 @@
 package decorator;
 
 /**
- * Created by Egor on 24.06.2016.
+ * Implementation of whip addition
  */
 public class Whip extends CondimentDecorator {
 
     private Beverage beverage;
 
+    /**
+     * It create an instance of addition
+     * @param beverage instance of beverage
+     */
     public Whip(Beverage beverage) {
         this.beverage = beverage;
     }
+
+    /**
+     * Return a string which describe beverage with whip addition and other additions
+     * @return
+     */
     @Override
     public String getDescription() {
         return beverage.getDescription() + ", whip";
     }
 
+
+    /**
+     * It calculate price of beverage with addition
+     * @return
+     */
     @Override
     public double cost() {
         return 0.65 + beverage.cost();
